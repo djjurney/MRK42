02b58fc845e4d7e0832db5c81928ff57af57b7a5
David Jurney <djurney@cnm.edu>
2023-02-28 13:14:27 -0700

    Added HTMLfile

---
+++ build/precache-manifest.5170ca0a6d30d677f355dc88bd399c17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6261dcc833c66c613c9",
+    "url": "/static/css/main.d3171988.chunk.css"
+  },
+  {
+    "revision": "e6261dcc833c66c613c9",
+    "url": "/static/js/main.645b0cf7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3248047fe2fb2119e923",
+    "url": "/static/js/2.ee7f18c8.chunk.js"
+  },
+  {
+    "revision": "67dcd06aa566a7977d042420b622ee27",
+    "url": "/index.html"
+  }
+];
